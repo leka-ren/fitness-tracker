eb7194d7ae92fb0d14e4caf097de814bb365bfe8
Leka <leka@Antons-MacBook-Pro.local>
2025-10-24 17:19:30 +0300

    implemented DayActionInfo function

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -2,6 +2,7 @@ package daysteps
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 	"time"
@@ -47,4 +48,21 @@ func parsePackage(data string) (int, time.Duration, error) {
 
 func DayActionInfo(data string, weight, height float64) string {
 	// TODO: реализовать функцию
+	steps, walkDuration, errorDataParse := parsePackage(data)
+
+	if errorDataParse != nil {
+		fmt.Println(errorDataParse)
+		return ""
+		// в тз написано: "Проверить, чтобы количество шагов было больше 0. В противном случае вернуть пустую строку.",
+		// но если я сделаю тут проверку на колчество шагов, она никогда не отработает в случае если шагов будет 0,
+		// потому что такая проверка есть в функции parsePackage, и она возвращает ошибку в случае если шагов 0 или меньше
+		// соответственно errorDataParse != nil будет true и функция отобразит ошибку и вернет пустую строку.
+	}
+	distanseInM := stepLength * float64(steps)
+	distanseInKm := distanseInM / mInKm
+	caloriesTotal := 0
+
+	res := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distanseInKm, caloriesTotal)
+
+	return res
 }
