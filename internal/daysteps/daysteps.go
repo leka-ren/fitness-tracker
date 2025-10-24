d14231c5790c5bb9c28994196116db152ec9f8d3
Leka <leka@Antons-MacBook-Pro.local>
2025-10-24 16:33:20 +0300

    implemented parsePackage function

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,9 @@
 package daysteps
 
 import (
+	"errors"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -13,6 +16,33 @@ const (
 
 func parsePackage(data string) (int, time.Duration, error) {
 	// TODO: реализовать функцию
+
+	walkInform := strings.Split(data, ",")
+
+	if len(walkInform) != 2 {
+		return 0, 0, errors.New("incorrect data length, should be 2")
+	}
+
+	stepsCount, errorStepsConvert := strconv.Atoi(walkInform[0])
+	walkDuration, errorWalkDurationParse := time.ParseDuration(walkInform[1])
+
+	if stepsCount <= 0 {
+		return 0, 0, errors.New("incorrect steps count")
+	}
+
+	if walkDuration.Seconds() <= 0 {
+		return 0, 0, errors.New("incorrect walk duration")
+	}
+
+	if errorStepsConvert != nil {
+		return 0, 0, errorStepsConvert
+	}
+
+	if errorWalkDurationParse != nil {
+		return 0, 0, errorWalkDurationParse
+	}
+
+	return stepsCount, walkDuration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
